39238c3bbdb4377c28f85edda44c81a0e01c0fd9
Yogen Parekh <yogenparekh569@gmail.com>
2021-04-28 13:21:45 +0100

    BaseApplication DI

---
+++ app/src/main/java/com/yogenp/openweathercompose/presentation/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.yogenp.openweathercompose.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
