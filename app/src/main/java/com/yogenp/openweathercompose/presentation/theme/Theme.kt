91295bb655073a8b56af860e49f607740316bd40
Yogen Parekh <yogenparekh569@gmail.com>
2021-04-29 13:52:04 +0100

    Theme, Permission Update

---
+++ app/src/main/java/com/yogenp/openweathercompose/presentation/theme/Theme.kt
@@ -0,0 +1,49 @@
+package com.yogenp.openweathercompose.presentation.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+// Defining Custom Theme Attributes
+
+private val LightThemeColors = lightColors(
+    primary = Blue600,
+    primaryVariant = Blue400,
+    onPrimary = Color.White,
+    secondary = Blue300,
+    secondaryVariant = Teal300,
+    onSecondary = Color.White,
+    error = RedErrorDark,
+    onError = RedErrorLight,
+    background = Grey1,
+    onBackground = Color.White,
+    surface = Color.White,
+    onSurface = Black2,
+)
+
+private val DarkThemeColors = darkColors(
+    primary = Blue700,
+    primaryVariant = Color.White,
+    onPrimary = Color.White,
+    secondary = Color.Black,
+    onSecondary = Black3,
+    error = RedErrorLight,
+    background = Black3,
+    onBackground = Color.White,
+    surface = Black3,
+    onSurface = Color.White,
+)
+
+@Composable
+fun AppTheme(
+    darkTheme: Boolean,
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = if (darkTheme) DarkThemeColors else LightThemeColors
+    ) {
+        content()
+    }
+}
